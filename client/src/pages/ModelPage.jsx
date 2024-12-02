08ff74523370742581db3366915a128e13e6b487
Neasakiru <aleksanderb2001@wp.pl>
2024-12-02 15:06:57 +0100

    c

--- client/src/pages/ModelPage.jsx
+++ client/src/pages/ModelPage.jsx
@@ -18,16 +18,50 @@ function ModelPage(props) {
         }}
       >
         <Navbar />
-        <div className="buttons">
-          <button
-            className={show ? "specButton hidden" : "specButton"}
-            onClick={() => setShow(!show)}
-          >
-            Osiągi {props.name}
-          </button>
+        <div className="model">
+          <span className="span-name">{props.name}</span>
+          <span className="span-model">{props.model}</span>
+          <span className="span-year">{props.year}</span>
         </div>
+        <button
+          className={show ? "button hidden" : "button button-absolut"}
+          onClick={() => setShow(!show)}
+        >
+          Specyfikacja
+        </button>
         <div className={show ? "spec" : "spec hidden"}>
-          <button onClick={() => setShow(!show)}>&times</button>
+          <h2>-Specyfikacja-</h2>
+          <div className="content">
+            <div>
+              <span className="placeholder">Moc: </span>
+              <span>{props.hp}</span>
+            </div>
+            <div>
+              <span className="placeholder">Moment Obrotowy: </span>
+              <span>{props.torque}</span>
+            </div>
+            <div>
+              <span className="placeholder">Przyspieszenie 0-100km/h:</span>
+              <span>{props.acceleration}</span>
+            </div>
+            <div>
+              <span className="placeholder">Prędkość Maksymalna:</span>
+              <span>{props.vMax}</span>
+            </div>
+            <div>
+              <span className="placeholder">Skrzynia Biegów: </span>
+              <span>{props.transmission}</span>
+            </div>
+            <div>
+              <span className="placeholder">Napęd: </span>
+              <span>{props.drive}</span>
+            </div>
+          </div>
+          <div className="button-spacer">
+            <button className="button" onClick={() => setShow(!show)}>
+              Zamknij
+            </button>
+          </div>
         </div>
       </div>
     );
